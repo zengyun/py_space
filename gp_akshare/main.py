0a9a0a02f71e778c23c7c15971ce3b41c36315ce
曾云 <zengyun@habby.com>
2025-04-02 17:19:02 +0800

    1

--- gp_akshare/main.py
+++ gp_akshare/main.py
@@ -1,5 +1,6 @@
 import akshare as ak
 stock_zh_a_spot_em_df = ak.stock_zh_a_spot_em()
+#stock_zh_a_spot_em_df = ak.stock_zh_a_spot()
 stock_zh_a_spot_em_df.to_json("all_items.json",orient="records", force_ascii=False, indent=4)
 stock_zh_a_spot_em_df.to_excel("all_items.xls",index=False,sheet_name="1",engine="openpyxl")
 print(stock_zh_a_spot_em_df)
